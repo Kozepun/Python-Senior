4471266ca08c31fae75371b46f871401c8b2f754
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 20:27:52 +0200

    Added 1 Class

--- Lesson2/Main.py
+++ Lesson2/Main.py
@@ -1,10 +1,12 @@
 from Character import Character
 from Lesson3.berserk import Berserk
 from Lesson3.knight import Knight
+from HomeWork2.Assasin import Assasin
 player1 = Character()
 player2 = Berserk("y", 100, 10 , 0)
 player3 = Knight("UUU", 100, 10, 10)
-
+player4 = Assasin("UWU", 100, 1, 0)
+'''
 print(player2)
 player1.attack(player2)
 print(player2)
@@ -14,6 +16,9 @@ player2.attack(player1)
 print(player1)
 
 player2.attack(player3)
+print(player3)
+'''
+player4.attack(player3)
 print(player3)
 
 '''
