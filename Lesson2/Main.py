83b58fc09297fe5669de45564f8c157448f009a4
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 20:16:09 +0200

    Added 2 Classes

--- Lesson2/Main.py
+++ Lesson2/Main.py
@@ -1,6 +1,22 @@
 from Character import Character
-
+from Lesson3.berserk import Berserk
+from Lesson3.knight import Knight
 player1 = Character()
+player2 = Berserk("y", 100, 10 , 0)
+player3 = Knight("UUU", 100, 10, 10)
+
+print(player2)
+player1.attack(player2)
+print(player2)
+
+print(player2.count_additional_damage())
+player2.attack(player1)
+print(player1)
+
+player2.attack(player3)
+print(player3)
+
+'''
 player2 = Character(name = "Tarakan", health= 20000, damage= 1)
 player3 = Character(name = "Nadya", health= 1000, damage= 100, defence= 1000)
 while player3.is_alive() & player2.is_alive():
@@ -8,3 +24,4 @@ while player3.is_alive() & player2.is_alive():
     player3.attack(player2)
     print(player2)
     print(player3)
+'''
