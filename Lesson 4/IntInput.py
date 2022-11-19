0a4c3f4c771343ec9426f63c931fa25f2cf362d8
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 12:23:42 +0200

    Init

--- Lesson 4/IntInput.py
+++ Lesson 4/IntInput.py
@@ -1,14 +1,13 @@
 def int_inp(message):
-    try:
-        inp = int(input(message))
-    except ValueError:
-        print("Please Enter Number")
-        return int_inp(message)
-    except Exception as error:
-        print(f"Error: {error}")
-        return int_inp(message)
-    else:
-        return inp
+    while True:
+        try:
+            inp = int(input(message))
+        except ValueError:
+            print("Please Enter Number")
+        except Exception as error:
+            print(f"Error: {error}")
+        else:
+            return inp
 a = int_inp("1")
 b = int_inp("2")
 
